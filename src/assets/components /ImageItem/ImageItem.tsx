4c4f0f7b10ac8e2d04c9f99ee5a8881538c22e38
huhhell <vika20020807@mail.ru>
2024-04-16 17:48:07 +0700

    add imageItem component

---
+++ src/assets/components /ImageItem/ImageItem.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+
+interface Props {
+    image: string;
+    name: string;
+}
+
+export default function ImageItem({image, name}: Props) {
+
+
+    return <Container>
+        <ImageContainer>
+            <Image src={image} />
+        </ImageContainer>
+        <Name>{name}</Name>
+    </Container>
+}
+
+const Container = styled.li`
+    background: transparent linear-gradient(140deg, #FFF7FC66 0%, #FFFFFF66 0%, #FFFFFF00 100%) 0% 0% no-repeat padding-box;
+    box-shadow: inset 3px 3px 6px #EEE5E9;
+    padding: 14px;
+    border-radius: 30px;
+    backdrop-filter: blur(20px);
+`
+
+const ImageContainer = styled.div`
+    width: 100%;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 20px;
+    overflow: hidden;
+    margin-bottom: 20px;
+    aspect-ratio: 1 / 1;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+
+const Name = styled.h4`
+    color: #fff;
+    font-size: 35px;
+    line-height: 120%;
+`
+
