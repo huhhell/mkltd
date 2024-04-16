68dd0eb2aff865fe1e3be339bf0c761cb94a0db3
huhhell <vika20020807@mail.ru>
2024-04-16 17:47:45 +0700

    add header component

---
+++ src/assets/components /Header/Header.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+
+interface Props {
+    isSignIn: boolean
+}
+export default function Header({isSignIn}: Props) {
+
+    return <Container>
+        <Logo>OneAI</Logo>
+        {!isSignIn && <SignInButton>Sign in</SignInButton>}
+    </Container>
+}
+
+const Container = styled.header`
+    width: 100%;
+    padding: 20px 35px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #fff;
+    border-radius: 0 0 50px 50px;
+    margin-bottom: 40px;
+`
+
+const Logo = styled.h2`
+    font-size: 35px;
+    font-weight: 600;
+    line-height: 122%;
+`
+
+const SignInButton = styled.button`
+    padding: 5px 20px;
+    color: #6600FF;
+    font-size: 19px;
+    font-weight: 500;
+    line-height: 130%;
+    border: 2px solid #6600FF;
+    box-shadow: 0 0 10px #6600ff;
+    -webkit-border-radius: 50px;
+    -moz-border-radius: 50px;
+    border-radius: 50px;
+`
